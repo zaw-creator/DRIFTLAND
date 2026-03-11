f77cfb867549473593d8837eba50c95ff9b5dcd7
zaw-creator <74670055+zaw-creator@users.noreply.github.com>
2026-03-11 14:47:09 +0000

    Update upload.js

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -3,6 +3,13 @@ const path = require("path");
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 // Cloudinary storage configuration
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
